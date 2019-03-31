11b02277b39e07633d85a53a9b79d720cce7e97e
ggya010 <ggya010@sina.com>
2019-03-31 21:30:01 +0800

    add some label

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ s_sheet = rs.get_sheet_by_name('Sheet1')
 d_sheet = wd.get_sheet_by_name('Sheet1')
 dmax_col = d_sheet.max_column
 maxrow = s_sheet.max_row
-print(s_sheet.max_column)
-print((s_sheet['1']))
 srcdict = {}
+#read data in dict
 for i in range(2,maxrow+1):
     tmp = []
     city = s_sheet.cell(row=i, column=2).value
@@ -20,6 +19,7 @@ for key,value in srcdict.items():
     print(key)
     print(value)
 count = 0
+#write data
 for i in range(1,dmax_col,9):
     tagcity = d_sheet.cell(row=1,column=i).value.strip()
     if tagcity == None:
@@ -29,4 +29,5 @@ for i in range(1,dmax_col,9):
     else:
         d_sheet.cell(row=4,column=i+3,value=srcdict[tagcity][1])
         print(d_sheet.cell(row=4,column=i+3).value)
+#save data 
 wd.save('d.xlsx')
